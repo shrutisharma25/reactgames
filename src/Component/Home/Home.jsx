53afeaa8134f553d6c3d37adb1b09b947b8ac93a
Shruti Sharma <shruti1005sharma@gmail.com>
2024-02-18 00:32:03 +0530

    ok

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+
+function Home() {
+  return (
+    <div>
+      <Link to="/tetris"> Tetris </Link>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/Component/Tetris/Tetris.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Tetris() {
+  return (
+    <div>
+      Tetris
+    </div>
+  )
+}
+
+export default Tetris
